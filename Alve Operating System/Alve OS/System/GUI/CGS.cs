7b8ef664bf624f68042fcb4686a65c30f6ab735e
Valentin Charbonnier <valentinbreiz@gmail.com>
2017-08-18 20:05:39 +0200

    Tests.

--- Alve Operating System/Alve OS/System/GUI/CGS.cs
+++ Alve Operating System/Alve OS/System/GUI/CGS.cs
@@ -11,41 +11,13 @@ namespace Alve_OS.System.GUI
 
         public static void Start()
         {
-            canvas = FullScreenCanvas.GetFullScreenCanvas();
-            canvas.Clear(Color.Blue);
+            Mode screenmode = new Mode(1366, 768, ColorDepth.ColorDepth32);
+            canvas = FullScreenCanvas.GetFullScreenCanvas(screenmode);
+            canvas.Clear(Color.White);
 
             try
             {
-                /* A red Point */
-                Pen pen = new Pen(Color.Red);
-                canvas.DrawPoint(pen, 69, 69);
 
-                /* A GreenYellow horizontal line */
-                pen.Color = Color.GreenYellow;
-                canvas.DrawLine(pen, 250, 100, 400, 100);
-
-                /* An IndianRed vertical line */
-                pen.Color = Color.IndianRed;
-                canvas.DrawLine(pen, 350, 150, 350, 250);
-
-                /* A MintCream diagonal line */
-                pen.Color = Color.MintCream;
-                canvas.DrawLine(pen, 250, 150, 400, 250);
-
-                /* A PaleVioletRed rectangle */
-                pen.Color = Color.PaleVioletRed;
-                canvas.DrawRectangle(pen, 350, 350, 80, 60);
-
-                Console.ReadKey();
-
-                /* Let's try to change mode...*/
-                canvas.Mode = new Mode(800, 600, ColorDepth.ColorDepth32);
-
-                /* A LimeGreen rectangle */
-                pen.Color = Color.LimeGreen;
-                canvas.DrawRectangle(pen, 450, 450, 80, 60);
-
-                AlveCanvas.DrawString(100, 100, "sampletexte", 0x000000);
 
                 Console.ReadKey();
 
